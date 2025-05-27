48db4f3765fe8ef925a4bbc751159d944b1a8c2f
Olga Mazurova <vsilena@mail.ru>
2025-05-27 10:55:50 +0300

    Написаны модели Review и Comment.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 User = get_user_model()
@@ -16,9 +17,30 @@ class Title(models.Model):
     pass
 
 
-class Comment(models.Model):
-    pass
-
-
 class Review(models.Model):
-    pass
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews'
+    )
+    text = models.TextField()
+    score = models.SmallIntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)]
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
