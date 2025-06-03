81d89c80079f2e46da820f0ca9ca4e933c1976f5
Olga Mazurova <vsilena@mail.ru>
2025-06-03 17:41:10 +0300

    Написана расширенная админка для всех моделей.

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -3,5 +3,6 @@ from rest_framework.exceptions import ValidationError
 
 
 def year_validator(value):
+    """Валидатор проверяет, что указан год не больше текущего."""
     if value > timezone.now().year:
         raise ValidationError('Год не может быть больше текущего!')
